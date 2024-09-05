4ac1d84ab8007f45ce6d103b5091af19520daa5f
Syed Abdul Rahim <abdulrahim555@hotmail.com>
2024-09-06 03:19:15 +0800

    Initial Commit

---
+++ Seabay/SeabayApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeabayApp.swift
+//  Seabay
+//
+//  Created by Syed Abdul Rahim Bin Syed Omar on 6/9/24.
+//
+
+import SwiftUI
+
+@main
+struct SeabayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
